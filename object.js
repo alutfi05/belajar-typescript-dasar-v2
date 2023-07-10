ffb524f4bc6b81b3b2d3ecd71aa1e03aaeca5bd0
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-10 14:32:42 +0700

    learning object and type alias in typescript

--- object.js
+++ object.js
@@ -1,32 +1,18 @@
-const User = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
     name: 'Lutfi',
     email: 'lutfi@gmail.com',
     isActive: true,
 };
-
 // const createUser = ({ name: string, isPaid: boolean }) => {};
-
 // let newUser = { name: 'Lutfi', isPaid: false, email: 'lutfi@gmail.com' };
 // createUser(newUser);
-
 // bad syntax
-const createCourse = (): { name: string; price: number } => {
+var createCourse = function () {
     return { name: 'Typescript', price: 399 };
 };
-
-// type alias
-type User = {
-    name: string;
-    email: string;
-    isActive: boolean;
-};
-
-const createUser = (user: User): User => {
+var createUser = function (user) {
     return { name: user.name, email: user.email, isActive: user.isActive };
 };
-
-console.log(
-    createUser({ name: 'Lutfi', email: 'lutfi@gmail.com', isActive: false })
-);
-
-export {};
+console.log(createUser({ name: 'Lutfi', email: 'lutfi@gmail.com', isActive: false }));
