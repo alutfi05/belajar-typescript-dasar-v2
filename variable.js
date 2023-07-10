35323576d9afe5542287cffd397757f20e30229a
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-10 10:58:40 +0700

    learning primitive data types (string, number, boolean)  with best practice in typescript

--- variable.js
+++ variable.js
@@ -1,2 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = 'Hello Lutfi';
+greetings.toLowerCase();
 console.log(greetings);
+// number
+var userId = 334455.5;
+// let userId = 334455.5; // best practice
+userId.toFixed();
+console.log(userId);
+// boolean
+var isLoggedIn = false;
+// let isLoggedIn = false; // best practice
+console.log(isLoggedIn);
--- variable.ts
+++ variable.ts
@@ -1,7 +1,19 @@
 let greetings: string = 'Hello Lutfi';
 
-let myNumber: number = 8;
-
+greetings.toLowerCase();
 console.log(greetings);
+
+// number
+let userId: number = 334455.5;
+// let userId = 334455.5; // best practice
+userId.toFixed();
+
+console.log(userId);
+
+// boolean
+let isLoggedIn: boolean = false;
+// let isLoggedIn = false; // best practice
+
+console.log(isLoggedIn);
 
 export {};
